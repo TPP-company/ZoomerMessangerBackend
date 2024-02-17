361545e0ed5820b1d7f22df98f808fa1eee78e1e
Maksim Vorobev <maks233online@gmail.com>
2024-02-18 01:19:33 +0300

    Тест запрета пуша в main

--- src/ZM.Infrastructure/Persistence/Identity/AuthDbContext.cs
+++ src/ZM.Infrastructure/Persistence/Identity/AuthDbContext.cs
@@ -9,8 +9,7 @@ internal class AuthDbContext : IdentityDbContext<AuthUser, AuthRole, Guid>
     public const string DefaultSchema = "identity";
 
     public AuthDbContext(DbContextOptions options) : base(options)
-    {
-    }
+    { }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
