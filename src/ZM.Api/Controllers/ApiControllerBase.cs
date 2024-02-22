c22cd093243f3de28a32a65abe0f538a2dc16885
Ferchke17 <ferdavs@naver.com>
2024-02-22 23:57:48 +0900

    Добавил контроллер для обновление информацию о юзере

--- src/ZM.Api/Controllers/ApiControllerBase.cs
+++ src/ZM.Api/Controllers/ApiControllerBase.cs
@@ -7,8 +7,6 @@ namespace ZM.Api.Controllers;
 [RoutePrefix("api/")]
 public abstract class ApiControllerBase : ControllerBase
 {
-#pragma warning disable CS8618 // ¬±¬à¬Ý¬Ö, ¬ß¬Ö ¬Õ¬à¬á¬å¬ã¬Ü¬Ñ¬ð¬ë¬Ö¬Ö ¬Ù¬ß¬Ñ¬é¬Ö¬ß¬Ú¬ñ NULL, ¬Õ¬à¬Ý¬Ø¬ß¬à ¬ã¬à¬Õ¬Ö¬â¬Ø¬Ñ¬ä¬î ¬Ù¬ß¬Ñ¬é¬Ö¬ß¬Ú¬Ö, ¬à¬ä¬Ý¬Ú¬é¬ß¬à¬Ö ¬à¬ä NULL, ¬á¬â¬Ú ¬Ó¬í¬ç¬à¬Õ¬Ö ¬Ú¬Ù ¬Ü¬à¬ß¬ã¬ä¬â¬å¬Ü¬ä¬à¬â¬Ñ. ¬£¬à¬Ù¬Þ¬à¬Ø¬ß¬à, ¬ã¬ä¬à¬Ú¬ä ¬à¬Ò¬ì¬ñ¬Ó¬Ú¬ä¬î ¬á¬à¬Ý¬Ö ¬Ü¬Ñ¬Ü ¬Õ¬à¬á¬å¬ã¬Ü¬Ñ¬ð¬ë¬Ö¬Ö ¬Ù¬ß¬Ñ¬é¬Ö¬ß¬Ú¬ñ NULL.
-    private ISender _sender;
-#pragma warning restore CS8618 // ¬±¬à¬Ý¬Ö, ¬ß¬Ö ¬Õ¬à¬á¬å¬ã¬Ü¬Ñ¬ð¬ë¬Ö¬Ö ¬Ù¬ß¬Ñ¬é¬Ö¬ß¬Ú¬ñ NULL, ¬Õ¬à¬Ý¬Ø¬ß¬à ¬ã¬à¬Õ¬Ö¬â¬Ø¬Ñ¬ä¬î ¬Ù¬ß¬Ñ¬é¬Ö¬ß¬Ú¬Ö, ¬à¬ä¬Ý¬Ú¬é¬ß¬à¬Ö ¬à¬ä NULL, ¬á¬â¬Ú ¬Ó¬í¬ç¬à¬Õ¬Ö ¬Ú¬Ù ¬Ü¬à¬ß¬ã¬ä¬â¬å¬Ü¬ä¬à¬â¬Ñ. ¬£¬à¬Ù¬Þ¬à¬Ø¬ß¬à, ¬ã¬ä¬à¬Ú¬ä ¬à¬Ò¬ì¬ñ¬Ó¬Ú¬ä¬î ¬á¬à¬Ý¬Ö ¬Ü¬Ñ¬Ü ¬Õ¬à¬á¬å¬ã¬Ü¬Ñ¬ð¬ë¬Ö¬Ö ¬Ù¬ß¬Ñ¬é¬Ö¬ß¬Ú¬ñ NULL.
+    private ISender _sender = null!;
     protected ISender Sender => _sender ??= HttpContext.RequestServices.GetRequiredService<ISender>();
 }
